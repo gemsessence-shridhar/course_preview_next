07c72d99549c98e83378db6d7861fa7a06693500
Shridhar Gupta <shridhar.gupta@gemsessence.com>
2025-01-16 12:29:28 +0530

    skip es lint for now

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true, // Disable ESLint during builds
+    },
+  };
+  
+  module.exports = nextConfig;
+  
